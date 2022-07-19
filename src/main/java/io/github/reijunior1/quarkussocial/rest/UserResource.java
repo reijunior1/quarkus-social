7398cf99e59ad836b1afe6dc62c9e0fa18e7b6a7
Reinaldo Couto <reijunior1@hotmail.com>
2022-07-19 16:39:27 -0300

    Implementation CRUD

--- src/main/java/io/github/reijunior1/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/reijunior1/quarkussocial/rest/UserResource.java
@@ -1,8 +1,11 @@
 package io.github.reijunior1.quarkussocial.rest;
 
 import io.github.reijunior1.quarkussocial.domain.model.User;
+import io.github.reijunior1.quarkussocial.domain.repository.UserRepository;
 import io.github.reijunior1.quarkussocial.rest.dto.CreateUserRequest;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -13,20 +16,57 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
+    private final UserRepository repository;
+
+    @Inject
+    public UserResource(UserRepository repository) {
+        this.repository = repository;
+    }
+
     @POST
     @Transactional
     public Response createUser(CreateUserRequest userRequest){
+
         User user = new User();
         user.setAge(userRequest.getAge());
         user.setName(userRequest.getName());
 
-        user.persist();
+        repository.persist(user);
 
         return Response.ok(user).build();
     }
 
     @GET
     public Response listAllUsers(){
-        return Response.ok().build();
+        PanacheQuery<User> query =  repository.findAll();
+        return Response.ok(query.list()).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response deleteUser(@PathParam("id") Long id){
+        User user = repository.findById(id);
+
+        if(user != null){
+            repository.delete(user);
+            return Response.ok().build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData){
+        User user = repository.findById(id);
+
+        if(user != null){
+            user.setName(userData.getName());
+            user.setAge(userData.getAge());
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
