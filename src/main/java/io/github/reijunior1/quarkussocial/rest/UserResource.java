5fb7699d9238cdc5f7341217b0b508d54798f692
Reinaldo Couto <reijunior1@hotmail.com>
2022-07-20 12:05:32 -0300

    Validations API

--- src/main/java/io/github/reijunior1/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/reijunior1/quarkussocial/rest/UserResource.java
@@ -3,37 +3,52 @@ package io.github.reijunior1.quarkussocial.rest;
 import io.github.reijunior1.quarkussocial.domain.model.User;
 import io.github.reijunior1.quarkussocial.domain.repository.UserRepository;
 import io.github.reijunior1.quarkussocial.rest.dto.CreateUserRequest;
+import io.github.reijunior1.quarkussocial.rest.dto.ResponseError;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import javax.validation.Constraint;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Set;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
-    private final UserRepository repository;
+    private UserRepository repository;
+    private Validator validator;
 
     @Inject
-    public UserResource(UserRepository repository) {
+    public UserResource(UserRepository repository, Validator validator) {
         this.repository = repository;
+        this.validator = validator;
     }
 
     @POST
     @Transactional
     public Response createUser(CreateUserRequest userRequest){
 
-        User user = new User();
-        user.setAge(userRequest.getAge());
-        user.setName(userRequest.getName());
+        Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
+        if(!violations.isEmpty()){
 
+            return  ResponseError.createFromValidation(violations)
+                    .withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
+        }
+
+        User user = new User();
+        user.setName(userRequest.getName());
+        user.setAge(userRequest.getAge());
         repository.persist(user);
 
-        return Response.ok(user).build();
+        return Response.status(Response.Status.CREATED.getStatusCode())
+        .entity(user)
+        .build();
     }
 
     @GET
@@ -50,7 +65,7 @@ public class UserResource {
 
         if(user != null){
             repository.delete(user);
-            return Response.ok().build();
+            return Response.noContent().build();
         }
 
         return Response.status(Response.Status.NOT_FOUND).build();
@@ -65,7 +80,7 @@ public class UserResource {
         if(user != null){
             user.setName(userData.getName());
             user.setAge(userData.getAge());
-            return Response.ok().build();
+            return Response.noContent().build();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
     }
