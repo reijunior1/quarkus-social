3b86a97fd4e1b370637a99a295771b966b123789
Reinaldo Couto <reijunior1@hotmail.com>
2022-07-20 16:49:29 -0300

    implementation Lombok

--- src/main/java/io/github/reijunior1/quarkussocial/domain/model/User.java
+++ src/main/java/io/github/reijunior1/quarkussocial/domain/model/User.java
@@ -1,12 +1,11 @@
 package io.github.reijunior1.quarkussocial.domain.model;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-
+import lombok.*;
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User  {
 
     @Id
@@ -17,47 +16,4 @@ public class User  {
     @Column(name = "age")
     private Integer age;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (!Objects.equals(id, user.id)) return false;
-        if (!Objects.equals(name, user.name)) return false;
-        return Objects.equals(age, user.age);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (age != null ? age.hashCode() : 0);
-        return result;
-    }
 }
--- src/main/java/io/github/reijunior1/quarkussocial/rest/dto/CreateUserRequest.java
+++ src/main/java/io/github/reijunior1/quarkussocial/rest/dto/CreateUserRequest.java
@@ -1,28 +1,14 @@
 package io.github.reijunior1.quarkussocial.rest.dto;
 
+import lombok.Data;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Data
 public class CreateUserRequest {
 
     @NotBlank(message = "Name is required")
     private String name;
     @NotNull(message = "Age is required")
     private Integer age;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
 }
