3f478f94c2dc556a9ba8ad7bd5f06fec31edee1f
Reinaldo Couto <reijunior1@hotmail.com>
2022-07-25 16:11:56 -0300

    Implementation API DELETE Followers

--- src/main/java/io/github/reijunior1/quarkussocial/domain/repository/FollowerRepository.java
+++ src/main/java/io/github/reijunior1/quarkussocial/domain/repository/FollowerRepository.java
@@ -27,4 +27,12 @@ public class FollowerRepository implements PanacheRepository<Follower> {
         return query.list();
     }
 
+    public void deleteByFollowerAndUser(Long followerId, Long userId) {
+       var params = Parameters
+               .with("userId", userId)
+               .and("followerId", followerId)
+               .map();
+
+        delete("follower.id = :followerId and user.id = :userId", params);
+    }
 }
